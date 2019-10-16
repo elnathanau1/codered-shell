60bf38c5d4215b15b8c16f0c6b01baf7df88eff1
elnathanau1 <elnathanau1@gmail.com>
2019-10-15 23:46:12 -0500

    finished delete-league

--- src/main/java/com/eau/codered/coderedshell/commands/LeagueCommands.java
+++ src/main/java/com/eau/codered/coderedshell/commands/LeagueCommands.java
@@ -2,6 +2,7 @@ package com.eau.codered.coderedshell.commands;
 
 import com.eau.codered.coderedshell.entities.LeagueEntity;
 import com.eau.codered.coderedshell.entities.TeamEntity;
+import com.eau.codered.coderedshell.services.DraftService;
 import com.eau.codered.coderedshell.services.LeagueService;
 import com.eau.codered.coderedshell.services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
 import java.util.List;
+import java.util.Scanner;
 
 @ShellComponent
 public class LeagueCommands {
@@ -18,6 +20,9 @@ public class LeagueCommands {
 
     @Autowired
     TeamService teamService;
+
+    @Autowired
+    DraftService draftService;
 
     @ShellMethod(value = "Create a league", key = "create-league")
     public String createLeague(@ShellOption(value = {"-n", "--name"}) String name,
@@ -50,5 +55,26 @@ public class LeagueCommands {
             );
         }
         return stringBuffer.toString();
+    }
+
+    @ShellMethod(value = "Delete a league", key = "delete-league")
+    public String deleteLeague(@ShellOption(value = {"-n", "--name"}) String name) {
+        // confirm with user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This will delete all info connected to the league. Continue? (y/n)");
+        String userInput = scanner.next();
+
+        if (!userInput.equals("y")) {
+            return "delete-league cancelled.";
+        }
+
+        LeagueEntity leagueEntity = leagueService.getLeagueByName(name);
+
+        if (!teamService.deleteTeamsInLeague(leagueEntity) || !draftService.cleanDraft(leagueEntity)|| !leagueService.deleteLeague(name)) {
+            return "Failed to delete league";
+        }
+
+        return "Successfully deleted league.";
+
     }
 }
